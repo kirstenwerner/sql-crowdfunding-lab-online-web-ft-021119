5628ca6ae3fc4c2ddfd66108d43e669afd02c339
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-20 19:47:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-("project_1", "category", 1000, "today", "tomorrow")
-("project_2", "category", 1000, "today", "tomorrow")
-("project_3", "category", 1000, "today", "tomorrow")
-("project_4", "category", 1000, "today", "tomorrow")
-("project_5", "category", 1000, "today", "tomorrow")
-("project_6", "category", 1000, "today", "tomorrow")
-("project_7", "category", 1000, "today", "tomorrow")
-("project_8", "category", 1000, "today", "tomorrow")
-("project_9", "category", 1000, "today", "tomorrow")
+("project_1", "category", 1000, "today", "tomorrow"),
+("project_2", "category", 1000, "today", "tomorrow"),
+("project_3", "category", 1000, "today", "tomorrow"),
+("project_4", "category", 1000, "today", "tomorrow"),
+("project_5", "category", 1000, "today", "tomorrow"),
+("project_6", "category", 1000, "today", "tomorrow"),
+("project_7", "category", 1000, "today", "tomorrow"),
+("project_8", "category", 1000, "today", "tomorrow"),
+("project_9", "category", 1000, "today", "tomorrow"),
 ("project_10", "category", 1000, "today", "tomorrow");
